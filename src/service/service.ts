8626893d6e3e000807a356c69ddc51a45c48463a
Artem Beresnev <artembert@gmail.com>
2020-01-27 00:40:15 +0300

    Service: Handle user input

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,1 +1,12 @@
-console.log(`Hello world`);
+const Cli = require(`./cli`);
+const {DEFAULT_COMMAND, USER_ARGV_INDEX, ExitCode} = require(`../constants`);
+
+const userArguments: string[] = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+
+if (userArguments.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+Cli[userCommand].run(userArguments.slice(1));
