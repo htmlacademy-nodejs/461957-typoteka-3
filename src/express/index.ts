6f8511f7dab3cb8565ac4059cf8bc19d68a8b240
Artem Beresnev <artembert@gmail.com>
2020-03-15 12:37:00 +0300

    [module3-task1] Front: Routes: add search

--- src/express/index.ts
+++ src/express/index.ts
@@ -5,14 +5,15 @@ const express = require(`express`);
 const mainPageRouter = require(`./routes/main-page`);
 const registerRouter = require(`./routes/register`);
 const loginRouter = require(`./routes/login`);
+const searchRouter = require(`./routes/search`);
 
 const app = express();
 
 app.use(`/`, mainPageRouter);
 app.use(`/register`, registerRouter);
 app.use(`/login`, loginRouter);
+app.use(`/search`, searchRouter);
 // app.use(`/my`, myRouter);
-// app.use(`/search`, searchRouter);
 // app.use(`/articles`, offersRouter);
 // app.use(`/500`, error500Router);
 // app.use(`/sign-up`, signUpRouter);
