179379f19fc9724378fc0ea587d754daa1beeb79
Artem Beresnev <artembert@gmail.com>
2020-03-15 12:50:16 +0300

    [module3-task1] Front: Routes: add my

--- src/express/index.ts
+++ src/express/index.ts
@@ -6,6 +6,7 @@ const mainPageRouter = require(`./routes/main-page`);
 const registerRouter = require(`./routes/register`);
 const loginRouter = require(`./routes/login`);
 const searchRouter = require(`./routes/search`);
+const myRouter = require(`./routes/my`);
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(`/`, mainPageRouter);
 app.use(`/register`, registerRouter);
 app.use(`/login`, loginRouter);
 app.use(`/search`, searchRouter);
-// app.use(`/my`, myRouter);
+app.use(`/my`, myRouter);
 // app.use(`/articles`, offersRouter);
 // app.use(`/500`, error500Router);
 // app.use(`/sign-up`, signUpRouter);
