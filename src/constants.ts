2523c641b12d09bf1285e11256add09c1a7546a5
Artem Beresnev <artembert@gmail.com>
2020-03-18 00:01:57 +0300

    [module3-task3] Service: fix constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,34 @@
+const DEFAULT_COMMAND = `--help`;
 const USER_ARGV_INDEX = 2;
 const ExitCode = {
-  error: 1,
-  success: 0
+  ERROR: 1,
+  SUCCESS: 0,
 };
+const HttpCode = {
+  OK: 200,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+const ContentType = {
+  PLAIN: `text/plain; charset=UTF-8`,
+  HTML: `text/html; charset=utf-8`,
+  CSS: `text/css; charset=utf-8`,
+};
+const DEFAULT_PORT = 3000;
 const SSR_PORT = 8080;
 const STATIC_DIR = `src/express/static`;
+const MOCK_FILE_PATH = `./mocks.json`;
 
 export = {
   USER_ARGV_INDEX,
   ExitCode,
   SSR_PORT,
-  STATIC_DIR
+  STATIC_DIR,
+  DEFAULT_PORT,
+  DEFAULT_COMMAND,
+  MOCK_FILE_PATH,
+  HttpCode,
+  ContentType,
 };
