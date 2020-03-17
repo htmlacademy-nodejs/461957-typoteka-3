e768c3647af63fd7d089863ab4b3454b82a7a6d0
Artem Beresnev <artembert@gmail.com>
2020-03-17 22:00:58 +0300

    [module3-task2] Front: Templates: comments

--- src/express/routes/my.ts
+++ src/express/routes/my.ts
@@ -3,6 +3,6 @@ const {Router} = require(`express`);
 const myRouter = new Router();
 
 myRouter.get(`/`, (req, res) => res.send(`/my`));
-myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+myRouter.get(`/comments`, (req, res) => res.render(`comments`));
 
 export = myRouter;
