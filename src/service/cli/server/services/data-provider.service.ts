ac0f46e6182bfbab80fabb60d31435b93941d90c
Artem Beresnev <artembert@gmail.com>
2020-06-14 16:44:52 +0300

    [module4-task1-rest-api] create data-provider.service

---
+++ src/service/cli/server/services/data-provider.service.ts
@@ -0,0 +1,31 @@
+import {Article} from "../../../../types/article";
+import {promises} from "fs";
+import {MOCK_FILE_PATH} from "../../../../constants-es6";
+
+export class DataProviderService {
+  public async getCategories(): Promise<string[] | null> {
+    try {
+      return Array.from(
+        new Set(
+          (await this.getArticles()).map(article => article.category).flat(1),
+        ),
+      );
+    } catch (e) {
+      console.error(`Failed to get categories`);
+      return null;
+    }
+  }
+
+  public async getArticles(): Promise<Article[]> {
+    try {
+      return JSON.parse(
+        await promises.readFile(MOCK_FILE_PATH, `utf-8`),
+      ) as Article[];
+    } catch (e) {
+      console.error(`Failed to get articles`);
+      return null;
+    }
+  }
+}
+
+export const dataProviderService = new DataProviderService();
