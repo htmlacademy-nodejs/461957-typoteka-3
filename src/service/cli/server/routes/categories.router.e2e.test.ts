adeb83e1d94a243c52fd9754f3aac015bed746c8
Artem Beresnev <artembert@gmail.com>
2020-06-14 19:33:30 +0300

    Categories: E2E: add get() method

---
+++ src/service/cli/server/routes/categories.router.e2e.test.ts
@@ -0,0 +1,18 @@
+import {App} from "../app";
+import {agent as request} from "supertest";
+
+describe(`Categories router`, () => {
+  let server;
+  beforeAll(async () => {
+    const app = new App();
+    await app.listen();
+    server = app.getServer();
+  });
+
+  describe(`get()`, () => {
+    test(`Should return code 200 when request categories`, async () => {
+      const res = await request(server).get(`/api/categories/`);
+      expect(res.status).toBe(200);
+    });
+  });
+});
