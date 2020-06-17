cadcb68f85016f54ee0ab3d4272b906d61d2bb99
Artem Beresnev <artembert@gmail.com>
2020-06-17 11:14:14 +0300

    Search: E2E: add tests

--- src/service/cli/server/routes/search.router.e2e.test.ts
+++ src/service/cli/server/routes/search.router.e2e.test.ts
@@ -14,5 +14,21 @@ describe(`Search router`, () => {
       const res = await request(server).get(`/api/search?query=test-query-string`);
       expect(res.status).toBe(200);
     });
+    test(`Should return code 200 when pass empty query string`, async () => {
+      const res = await request(server).get(`/api/search?query=`);
+      expect(res.status).toBe(200);
+    });
+    test(`Should return code 400 when don't pass query param`, async () => {
+      const res = await request(server).get(`/api/search`);
+      expect(res.status).toBe(400);
+    })
+    test(`Should return array when pass query string`, async () => {
+      const res = await request(server).get(`/api/search?query=%D1%84`);
+      expect(Array.isArray(res.body)).toBe(true);
+    })
+    test(`Should return empty array when pass empty query string`, async () => {
+      const res = await request(server).get(`/api/search?query=`);
+      expect(res.body.length).toBe(0);
+    })
   });
 });
