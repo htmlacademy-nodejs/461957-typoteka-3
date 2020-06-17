b6159319b643c20fa7af093d9f134b0014b99049
Artem Beresnev <artembert@gmail.com>
2020-06-17 20:04:54 +0300

    use articles.controller in articles.router

--- src/service/cli/server/routes/articles.router.ts
+++ src/service/cli/server/routes/articles.router.ts
@@ -1,8 +1,7 @@
 import {Router} from "express";
+import {articlesController} from "../controllers";
 
 // eslint-disable-next-line new-cap
 export const articlesRouter = Router();
 
-articlesRouter.get(`/`, (req, res) => {
-  res.send(`/api/articles`);
-});
+articlesRouter.get(`/`, (req, res) => articlesController.getArticles(req, res));
