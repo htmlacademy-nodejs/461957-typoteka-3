f22bfebde1a3bac38751a7dcfaa372291888104c
Artem Beresnev <artembert@gmail.com>
2020-06-17 21:31:22 +0300

    Articles: E2E: test getArticleById()

--- src/service/cli/server/routes/articles.router.e2e.test.ts
+++ src/service/cli/server/routes/articles.router.e2e.test.ts
@@ -2,10 +2,14 @@ import {App} from "../app";
 import {agent as request} from "supertest";
 import {Application} from "express";
 import * as http from "http";
+import {Article} from "../../../../types/article";
+
+const invalidArticleId = `invalid-article-id`;
+const validArticleId = `-H91UO1mzYQSeSGK2rxWC`;
 
 describe(`Articles router`, () => {
   let server: Application;
-  let httpServer: http.Server
+  let httpServer: http.Server;
   beforeAll(async () => {
     const app = new App();
     httpServer = app.listen();
@@ -13,7 +17,7 @@ describe(`Articles router`, () => {
   });
   afterAll(async () => {
     httpServer.close();
-  })
+  });
 
   describe(`get()`, () => {
     test(`Should return code 200 when request articles`, async () => {
@@ -23,6 +27,28 @@ describe(`Articles router`, () => {
     test(`Should return an array`, async () => {
       const res = await request(server).get(`/api/articles/`);
       expect(Array.isArray(res.body)).toBe(true);
-    })
+    });
+  });
+
+  describe(`get() by id`, () => {
+    test(`Should return code 404 when request invalid id`, async () => {
+      const res = await request(server).get(`/api/articles/${invalidArticleId}`);
+      expect(res.status).toBe(404);
+    });
+    test(`Should return code 200 when request valid id`, async () => {
+      const res = await request(server).get(`/api/articles/${validArticleId}`);
+      expect(res.status).toBe(200);
+    });
+    test(`Should return code 200 when request valid id`, async () => {
+      const res = await request(server).get(`/api/articles/${validArticleId}`);
+      const responseKeys = Object.keys(res.body as Article) as string[];
+      expect(responseKeys).toContain(`id`);
+      expect(responseKeys).toContain(`title`);
+      expect(responseKeys).toContain(`createdDate`);
+      expect(responseKeys).toContain(`announce`);
+      expect(responseKeys).toContain(`fullText`);
+      expect(responseKeys).toContain(`category`);
+      expect(responseKeys).toContain(`comments`);
+    });
   });
 });
