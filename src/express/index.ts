61b9381dee33eec49442f97d2bec0565352cf135
Artem Beresnev <artembert@gmail.com>
2020-07-01 18:17:11 +0300

    Set 'JSX' as a view engine

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,28 +1,35 @@
 import {DEFAULT_SSR_PORT, STATIC_DIR} from "../constants-es6";
-import {mainPageRouter} from "./routes/main-page";
+// import {mainPageRouter} from "./routes/main-page";
+import {createEngine} from "express-react-views";
 
 const chalk = require(`chalk`);
 
 const express = require(`express`);
-const registerRouter = require(`./routes/register`);
-const loginRouter = require(`./routes/login`);
-const searchRouter = require(`./routes/search`);
-const myRouter = require(`./routes/my`);
-const categoriesRouter = require(`./routes/categories`);
-const articlesRouter = require(`./routes/articles`);
+// const registerRouter = require(`./routes/register`);
+// const loginRouter = require(`./routes/login`);
+// const searchRouter = require(`./routes/search`);
+// const myRouter = require(`./routes/my`);
+// const categoriesRouter = require(`./routes/categories`);
+// const articlesRouter = require(`./routes/articles`);
 
 const app = express();
-app.set(`views`, `src/express/templates/pages`);
-app.set(`view engine`, `pug`);
+// app.set(`views`, `src/express/templates/pages`);
+// app.set(`view engine`, `pug`);
+// app.use(express.static(STATIC_DIR));
+app.set(`views`, __dirname + `/views`);
+app.set(`view engine`, `jsx`);
+app.engine(`jsx`, createEngine());
 app.use(express.static(STATIC_DIR));
 
-app.use(`/`, mainPageRouter);
-app.use(`/register`, registerRouter);
-app.use(`/login`, loginRouter);
-app.use(`/search`, searchRouter);
-app.use(`/my`, myRouter);
-app.use(`/categories`, categoriesRouter);
-app.use(`/articles`, articlesRouter);
+app.get(`/`, (req, res) => res.render(`index`, {name: `Name`}));
+
+// app.use(`/`, mainPageRouter);
+// app.use(`/register`, registerRouter);
+// app.use(`/login`, loginRouter);
+// app.use(`/search`, searchRouter);
+// app.use(`/my`, myRouter);
+// app.use(`/categories`, categoriesRouter);
+// app.use(`/articles`, articlesRouter);
 // app.use(`/500`, error500Router);
 // app.use(`*`, error404Router);
 
