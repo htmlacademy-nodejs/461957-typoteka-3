f4e5506172fa9db747d21bea51f9e36663c095ed
Artem Beresnev <artembert@gmail.com>
2020-07-04 13:30:47 +0300

    SSR: Layout: add prop isBackgroundHidden

--- src/express/views/components/Layout/Layout.tsx
+++ src/express/views/components/Layout/Layout.tsx
@@ -1,34 +1,32 @@
-import React from "react";
+import React, {FunctionComponent, ReactNode} from "react";
 import PropTypes from "prop-types";
 
-export function Layout(props) {
-  return (
-    <html lang="ru">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="Description" content="Личный блог Типотека" />
-        <title>Типотека</title>
-        <link rel="preload" href="/fonts/Inter-Regular.woff2" as="font" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/Inter-Medium.woff2" as="font" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/SFProDisplay-Regular.woff2" as="font" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="/css/style.min.css" />
-        <base href="/" />
-      </head>
-
-      <body>
-        <div className="wrapper">
-          {props.header}
-          {props.children}
-          {props.footer}
-        </div>
-      </body>
-    </html>
-  );
+export interface LayoutProps {
+  isBackgroundHidden?: boolean;
+  header: ReactNode;
+  footer: ReactNode;
 }
 
-Layout.propTypes = {
-  header: PropTypes.node.isRequired,
-  children: PropTypes.node.isRequired,
-  footer: PropTypes.node.isRequired,
-};
+export const Layout: FunctionComponent<LayoutProps> = ({isBackgroundHidden, header, children, footer}) => (
+  <html lang="ru">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="Description" content="Личный блог Типотека" />
+      <title>Типотека</title>
+      <link rel="preload" href="/fonts/Inter-Regular.woff2" as="font" crossOrigin="anonymous" />
+      <link rel="preload" href="/fonts/Inter-Medium.woff2" as="font" crossOrigin="anonymous" />
+      <link rel="preload" href="/fonts/SFProDisplay-Regular.woff2" as="font" crossOrigin="anonymous" />
+      <link rel="stylesheet" href="/css/style.min.css" />
+      <base href="/" />
+    </head>
+
+    <body>
+      <div className={"wrapper" + (isBackgroundHidden ? " wrapper--nobackground" : "")}>
+        {header}
+        {children}
+        {footer}
+      </div>
+    </body>
+  </html>
+);
