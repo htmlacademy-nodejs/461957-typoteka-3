47c19be98b9f6b70cd4802316456bbfabaeaecc9
Artem Beresnev <artembert@gmail.com>
2020-07-04 14:26:02 +0300

    SSR: Create method getComments()

--- src/express/controllers/main-page.controller.ts
+++ src/express/controllers/main-page.controller.ts
@@ -1,7 +1,8 @@
 import axios, {AxiosStatic, AxiosResponse} from "axios";
 import {Article} from "../../types/article";
 import {ENV} from "../../shared/env/env";
-import {Routes} from "../../constants-es6";
+import {HttpCode, Routes} from "../../constants-es6";
+import {ArticleComment} from "../../types/article-comment";
 
 export class MainPageController {
   private requestService: AxiosStatic;
@@ -23,6 +24,38 @@ export class MainPageController {
         ...article,
         createdDate: new Date(Date.parse((article.createdDate as unknown) as string)),
       }));
+    } else {
+      console.error(response.data);
+      return null;
+    }
+  }
+
+  public async getComments(quantityOfArticles: number): Promise<ArticleComment[]> {
+    const articlesList = await this.getArticles();
+    if (articlesList === null) {
+      return null;
+    }
+    const comments = await Promise.all(
+      articlesList.slice(0, quantityOfArticles).map(article => this.getArticleComments(article.id)),
+    );
+    if (comments === null) {
+      return null;
+    }
+    return comments.flat(1);
+  }
+
+  private async getArticleComments(articleId: string): Promise<ArticleComment[]> {
+    let response: AxiosResponse<ArticleComment[]>;
+    try {
+      response = await this.requestService.get<ArticleComment[]>(
+        this.apiEndPoint + Routes.ARTICLES + `/` + articleId + Routes.COMMENTS,
+        {},
+      );
+    } catch (e) {
+      console.error(`error`, e);
+    }
+    if (response && response.status === HttpCode.OK) {
+      return response.data;
     } else {
       console.error(response.data);
       return null;
