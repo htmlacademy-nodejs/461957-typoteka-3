905ae7b463511ce4608b8c91b1e4c4002956c10e
Artem Beresnev <artembert@gmail.com>
2020-07-04 13:23:55 +0300

    SSR: Footer: add prop isLargeIndent

--- src/express/views/components/Footer/Footer.tsx
+++ src/express/views/components/Footer/Footer.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, {FunctionComponent} from "react";
 
-export function Footer() {
-  return (
-    <footer className="footer">
-      <a className="footer__copyright" href="#" aria-label="HTMLAcademy">
-        <img src="img/icons/HTMLAcademyLogo.svg" width="112" height="50" alt="HTMLAcademy" />
-      </a>
-      <span className="footer__dot"></span>
-      <p className="footer__text">Типотека, 2019</p>
-    </footer>
-  );
+interface FooterProps {
+  isLargeIndent?: boolean;
 }
+
+export const Footer: FunctionComponent<FooterProps> = ({isLargeIndent}) => (
+  <footer className={"footer" + (isLargeIndent ? " footer--padding" : "")}>
+    <a className="footer__copyright" href="#" aria-label="HTMLAcademy">
+      <img src="img/icons/HTMLAcademyLogo.svg" width="112" height="50" alt="HTMLAcademy" />
+    </a>
+    <span className="footer__dot"></span>
+    <p className="footer__text">Типотека, 2019</p>
+  </footer>
+);
