5c4415965743344ae1313fe826f63957c0d4c827
Artem Beresnev <artembert@gmail.com>
2020-07-10 23:42:13 +0300

    Uninstall "express-react-views"

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,6 +1,5 @@
 import {DEFAULT_SSR_PORT, STATIC_DIR} from "../constants-es6";
 import {mainPageRouter} from "./routes/main-page";
-import {createEngine} from "express-react-views";
 import express from "express";
 import chalk from "chalk";
 import * as path from "path";
@@ -16,12 +15,8 @@ import {ErrorPage404} from "./views/pages/ErrorPage404";
 // const searchRouter = require(`./routes/search`);
 // const categoriesRouter = require(`./routes/categories`);
 // const articlesRouter = require(`./routes/articles`);
-const templateExtension = ENV.NODE_ENV === `development` ? `tsx` : `js`;
 const port = ENV.SSR_PORT || DEFAULT_SSR_PORT;
 const app = express();
-app.set(`views`, path.join(__dirname, `views`));
-app.set(`view engine`, templateExtension);
-app.engine(templateExtension, createEngine());
 app.use(express.static(path.join(__dirname, STATIC_DIR)));
 
 app.use(`/`, mainPageRouter);
