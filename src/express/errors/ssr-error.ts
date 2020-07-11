ef0825fbc97401e446a83b016262a8f74f725833
Artem Beresnev <artembert@gmail.com>
2020-07-11 15:02:52 +0300

    SSR: handle errors in '/my' route

--- src/express/errors/ssr-error.ts
+++ src/express/errors/ssr-error.ts
@@ -1,6 +1,7 @@
 export class SSRError extends Error {
   public statusCode: number;
   public shouldRedirect: boolean;
+  public errorPayload: any;
   constructor(config: Partial<SSRError> & {message: string}) {
     super(config.message);
     Object.assign(this, config);
--- src/express/routes/admin-publications.router.ts
+++ src/express/routes/admin-publications.router.ts
@@ -4,23 +4,44 @@ import {dataProviderService} from "../services/data-provider.service";
 import {streamPage} from "../utils/stream-page";
 import {AdminPublicationsPage} from "../views/pages/AdminPublicationsPage";
 import {AdminCommentsPage} from "../views/pages/AdminCommentsPage";
+import {SSRError} from "../errors/ssr-error";
 
 export const adminPublicationsRouter = Router();
 
-adminPublicationsRouter.get(`/`, async (req, res) => {
-  const articles = await dataProviderService.getArticles();
-  if (articles !== null) {
-    streamPage(res, AdminPublicationsPage, {articles});
-  } else {
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).send();
+adminPublicationsRouter.get(`/`, async (req, res, next) => {
+  try {
+    const articles = await dataProviderService.getArticles();
+    if (articles !== null) {
+      streamPage(res, AdminPublicationsPage, {articles});
+    } else {
+      next(new SSRError({message: `Failed to get Admin articles`, statusCode: HttpCode.INTERNAL_SERVER_ERROR}));
+    }
+  } catch (e) {
+    next(
+      new SSRError({
+        message: `Failed to get Admin articles`,
+        statusCode: HttpCode.INTERNAL_SERVER_ERROR,
+        errorPayload: e,
+      }),
+    );
   }
 });
 
-adminPublicationsRouter.get(`/comments`, async (req, res) => {
-  const listOfComments = await dataProviderService.getComments(3);
-  if (listOfComments !== null) {
-    streamPage(res, AdminCommentsPage, {listOfComments});
-  } else {
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).send();
+adminPublicationsRouter.get(`/comments`, async (req, res, next) => {
+  try {
+    const listOfComments = await dataProviderService.getComments(3);
+    if (listOfComments !== null) {
+      streamPage(res, AdminCommentsPage, {listOfComments});
+    } else {
+      next(new SSRError({message: `Failed to get Admin comments`, statusCode: HttpCode.INTERNAL_SERVER_ERROR}));
+    }
+  } catch (e) {
+    next(
+      new SSRError({
+        message: `Failed to get Admin comments`,
+        statusCode: HttpCode.INTERNAL_SERVER_ERROR,
+        errorPayload: e,
+      }),
+    );
   }
 });
