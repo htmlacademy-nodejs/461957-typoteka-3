4cc7ddbfbcaefc4268cb42a8b4db77c6ce962e42
Artem Beresnev <artembert@gmail.com>
2020-07-11 14:35:13 +0300

    SSR: MainPage router: handle error on request API

--- src/express/routes/main-page.ts
+++ src/express/routes/main-page.ts
@@ -1,16 +1,21 @@
-import {Router} from "express";
+import {NextFunction, Router} from "express";
 import {HttpCode} from "../../constants-es6";
 import {dataProviderService} from "../services/data-provider.service";
 import {streamPage} from "../utils/stream-page";
 import {MainPage} from "../views/pages/MainPage";
+import {SSRError} from "../errors/ssr-error";
 
 export const mainPageRouter = Router();
 
-mainPageRouter.get(`/`, async (req, res) => {
-  const articles = await dataProviderService.getArticles();
-  if (articles !== null) {
-    streamPage(res, MainPage, {articles});
-  } else {
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).send();
+mainPageRouter.get(`/`, async (req, res, next: NextFunction) => {
+  try {
+    const articles = await dataProviderService.getArticles();
+    if (articles !== null) {
+      streamPage(res, MainPage, {articles});
+    } else {
+      next(new SSRError({message: `Failed to load articles`, statusCode: HttpCode.INTERNAL_SERVER_ERROR}));
+    }
+  } catch (e) {
+    next(new SSRError({message: `Failed to load articles`, statusCode: HttpCode.INTERNAL_SERVER_ERROR}));
   }
 });
