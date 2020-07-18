f45f339e91a0e8ac837ded69895493eef2ab45e9
Artem Beresnev <artembert@gmail.com>
2020-07-18 20:53:03 +0300

    SSR: add missed imports

--- src/express/routes/articles.router.ts
+++ src/express/routes/articles.router.ts
@@ -1,6 +1,10 @@
 import {Router} from "express";
 import {streamPage} from "../utils/stream-page";
 import {NewArticlePage} from "../views/pages/NewArticlePage";
+import {SSRError} from "../errors/ssr-error";
+import {HttpCode} from "../../constants-es6";
+import {dataProviderService} from "../services/data-provider.service";
+import {ArticlePage} from "../views/pages/ArticlePage";
 
 export const articlesRouter = Router();
 
