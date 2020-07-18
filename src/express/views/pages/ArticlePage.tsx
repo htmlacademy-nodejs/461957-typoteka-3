86ec407d8fb17b6770705f55d17724d1daf6010a
Artem Beresnev <artembert@gmail.com>
2020-07-18 20:45:56 +0300

    SSR: create placeholder for NewArticlePage

---
+++ src/express/views/pages/ArticlePage.tsx
@@ -0,0 +1,17 @@
+import React, {FunctionComponent} from "react";
+import {LayoutFilled} from "../components/Layout/LayoutFilled";
+import {Article} from "../../../types/article";
+
+interface ArticlePageProps {
+  article?: Article;
+}
+
+export const ArticlePage: FunctionComponent<ArticlePageProps> = ({article}) => (
+  <LayoutFilled>
+    <main>
+      <section className="post">
+        <code>{JSON.stringify(article, undefined, 2)}</code>
+      </section>
+    </main>
+  </LayoutFilled>
+);
