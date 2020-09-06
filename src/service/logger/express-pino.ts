3b76a8613331eefca491980bd6916e74af7ed30e
Artem Beresnev <artembert@gmail.com>
2020-09-06 10:24:55 +0300

    [module4-task3] Log requests at the `debug` level

--- src/service/logger/express-pino.ts
+++ src/service/logger/express-pino.ts
@@ -1,4 +1,4 @@
 import pino from "express-pino-logger";
 import {getLogger} from "./manual-logger";
 
-export const expressPino = pino({logger: getLogger()});
+export const expressPino = pino({logger: getLogger(), useLevel: `debug`});
