a057a6931e6f2deb3ae4b866d31dc40c60ef917f
Artem Beresnev <artembert@gmail.com>
2020-09-18 20:00:35 +0300

    [module4-task3] Log request url

--- src/service/middlewares/logger/index.ts
+++ src/service/middlewares/logger/index.ts
@@ -1,2 +1,3 @@
 export {responseStatusCodeMiddleware} from "./response-status-code.middleware";
 export {assignLogFieldsMiddleware} from "./assign-log-fields.middleware";
+export {logRouteMiddleware} from "./log-route.middleware";
---
+++ src/service/middlewares/logger/log-route.middleware.ts
@@ -0,0 +1,9 @@
+import {NextFunction, Response} from "express";
+import {getLogger} from "../../logger";
+import {messageConstructor} from "../../logger/message-constructor";
+import {RequestExtended} from "../../models/types/request-extended";
+
+export function logRouteMiddleware(req: RequestExtended, res: Response, next: NextFunction): void {
+  getLogger().debug(messageConstructor(req.context.id, `Request URL: '${req.url}'`));
+  next();
+}
