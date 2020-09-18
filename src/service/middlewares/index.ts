025a32bb3e779f57bf822a58cac4fd74c94723c9
Artem Beresnev <artembert@gmail.com>
2020-09-18 12:44:58 +0300

    [module4-task3] Use named re-export instead of export all

--- src/service/middlewares/index.ts
+++ src/service/middlewares/index.ts
@@ -1,4 +1,4 @@
-export * from "./article-validator";
-export * from "./comment-validator";
-export * from "./response-status-code.middleware";
+export {newArticleValidator, existingArticleValidator} from "./article-validator";
+export {newCommentValidator} from "./comment-validator";
+export {responseStatusCodeMiddleware} from "./response-status-code.middleware";
 export {assignLogFieldsMiddleware} from "./assign-log-fields.middleware";
