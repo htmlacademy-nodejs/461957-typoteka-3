779201a3ef51580f500e1951df9f07ed967a0d26
Artem Beresnev <aberesnev@intermedia.net>
2020-11-04 00:33:06 +0300

    [module4-task3-part2]: Search page: fix styles

--- src/express/views/pages/SearchPage.tsx
+++ src/express/views/pages/SearchPage.tsx
@@ -16,9 +16,12 @@ export const SearchPage: FunctionComponent<SearchPageProps> = ({query, matches =
     <LayoutFilled>
       <main>
         <section className="search-page">
-          <div className={"search-page__wrapper" + (isEmpty ? " search-page__wrapper--empty" : "")}>
+          <div
+            className={"search-page__wrapper" + (isEmpty ? " search-page__wrapper--empty" : "")}
+            style={{minHeight: `610px`, height: `auto`}}
+          >
             <h1>Поиск</h1>
-            <div className="search search-page__form">
+            <div className="search search-page__form" style={{ paddingBottom: "20px" }}>
               <form action={endPoint} method="get">
                 <label>
                   <input type="text" name="query" placeholder="Что ищем?" defaultValue={query} />
