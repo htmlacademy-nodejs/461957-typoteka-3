269651a2659ba5e5d2c7292f646ad510ce484fee
Artem Beresnev <artembert@gmail.com>
2020-11-19 23:07:10 +0300

    [module5-task2]: DataBase: Add ';' and line break in the end of 'insertToTable' function

--- src/service/cli/generate-database-mock/sql-functions/insert-to-table.ts
+++ src/service/cli/generate-database-mock/sql-functions/insert-to-table.ts
@@ -6,5 +6,5 @@ type TableName = typeof TableNames[TableNameKey];
 export function insertToTable(table: TableName, values: string[]): string {
   return `INSERT INTO ${table} (${values
     .map(value => `'` + value.replace(/"/g, `\\"`).replace(/'/g, `\\'`) + `'`)
-    .toString()})`;
+    .toString()});\n`;
 }
