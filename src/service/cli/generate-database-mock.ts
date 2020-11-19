b1732d42296fc93697793320c6e6ee5157eba934
Artem Beresnev <artembert@gmail.com>
2020-11-19 23:12:00 +0300

    [module5-task2]: DataBase: Generate commands to fill categories

--- src/service/cli/generate-database-mock.ts
+++ src/service/cli/generate-database-mock.ts
@@ -1,12 +1,27 @@
 import yargs from "yargs/yargs";
+import {ICLIArguments} from "./generate-database-mock/models/cli-arguments.interface";
+import {insertToTable} from "./generate-database-mock/sql-functions/insert-to-table";
+import {appendToFile} from "./generate-database-mock/fs-functions/append-to-file";
+import {readTXTFile} from "./generate";
+import {MockFilePath, MockTextsFilePath, TableNames} from "../../constants-es6";
 
-function run(): void {
-  const argv = yargs(process.argv.slice(2)).options({
+function getCliArguments(): ICLIArguments {
+  return yargs(process.argv.slice(2)).options({
     database: {type: `string`, demandOption: true, desc: `Database name`},
     user: {type: `string`, demandOption: true, desc: `User who has access to database`},
     number: {type: `number`, desc: `Number of generated articles`, default: 3},
-  }).argv;
-  console.log(`argv`, argv);
+  }).argv as ICLIArguments;
 }
 
-run();
+getCliArguments();
+
+async function init(): Promise<void> {
+  const params: ICLIArguments = getCliArguments();
+  const categories = await readTXTFile(MockTextsFilePath.CATEGORIES);
+  for (const category of categories) {
+    const fillTableCategories = insertToTable(TableNames.CATEGORIES, [category]);
+    await appendToFile(MockFilePath.FILL_DATABASE_SQL_SCRIPT, fillTableCategories);
+  }
+}
+
+void init();
