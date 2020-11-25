9c0ebab33fb5e3ded77d0ce2c3cb2845ee40c7f1
Artem Beresnev <artembert@gmail.com>
2020-11-25 22:50:05 +0300

    [module5-task3]: Add one more query

--- data-base/queries.sql
+++ data-base/queries.sql
@@ -15,11 +15,32 @@ SELECT categories.id,
        categories.title,
        count(articles_categories.article_id) articles_count
 from categories
-         JOIN articles_categories on categories.id = articles_categories.category_id
+         INNER JOIN articles_categories on categories.id = articles_categories.category_id
 GROUP BY categories.id
 ORDER BY articles_count DESC;
 
 -- Список публикаций (идентификатор публикации, заголовок публикации, анонс публикации, дата публикации, имя и фамилия автора, контактный email, количество комментариев, наименование категорий). Сначала свежие публикации;
--- Солную информацию определённой публикации (идентификатор публикации, заголовок публикации, анонс, полный текст публикации, дата публикации, путь к изображению, имя и фамилия автора, контактный email, количество комментариев, наименование категорий);
+SELECT articles.id,
+       articles.title,
+       articles.announce,
+       articles.date,
+       concat(users.first_name, ' ', users.last_name) author,
+       users.email,
+       comments.count                                 comments_count,
+       string_agg(categories.title, ', ')             categories
+from articles
+         INNER JOIN users on articles.user_id = users.id
+         INNER JOIN articles_categories on articles.id = articles_categories.article_id
+         INNER JOIN categories on articles_categories.category_id = categories.id
+         LEFT JOIN
+     (
+         SELECT article_id, count(1) count
+         from comments
+         GROUP BY article_id
+     ) comments on articles.id = comments.article_id
+GROUP BY articles.id, comments_count, users.first_name, users.last_name, users.email, articles.date
+ORDER BY articles.date DESC;
+
+-- Полную информацию определённой публикации (идентификатор публикации, заголовок публикации, анонс, полный текст публикации, дата публикации, путь к изображению, имя и фамилия автора, контактный email, количество комментариев, наименование категорий);
 -- Список из 5 свежих комментариев (идентификатор комментария, идентификатор публикации, имя и фамилия автора, текст комментария);
 -- Список комментариев для определённой публикации (идентификатор комментария, идентификатор публикации, имя и фамилия автора, текст комментария). Сначала новые комментарии;
