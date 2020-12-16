6d1021e40362617b9443c4350045afd9f6cce5a7
Artem Beresnev <artembert@gmail.com>
2020-12-16 21:19:13 +0300

    [module6-templates-updates]: Add index.ts to routes/

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,17 +1,19 @@
 import {ClientRoutes, DEFAULT_SSR_PORT, STATIC_DIR} from "../constants-es6";
-import {mainPageRouter} from "./routes/main-page";
+import {
+  adminPublicationsRouter,
+  articlesRouter,
+  categoriesRouter,
+  mainPageRouter,
+  registrationRouter,
+  searchRouter,
+  signInRouter,
+} from "./routes";
 import express from "express";
 import chalk from "chalk";
 import * as path from "path";
 import {ENV} from "../shared/env/env";
-import {adminPublicationsRouter} from "./routes/admin-publications.router";
-import {articlesRouter} from "./routes/articles.router";
 import {notFoundMiddleware} from "./middlewares/not-found.middleware";
 import {errorHandlerMiddleware} from "./middlewares/error-handler.middleware";
-import {searchRouter} from "./routes/search.router";
-import {registrationRouter} from "./routes/registration.router";
-import {signInRouter} from "./routes/sign-in.router";
-import {categoriesRouter} from "./routes/categories.router";
 
 const port = ENV.SSR_PORT || DEFAULT_SSR_PORT;
 const app = express();
---
+++ src/express/routes/index.ts
@@ -0,0 +1,7 @@
+export {adminPublicationsRouter} from "./admin-publications.router";
+export {articlesRouter} from "./articles.router";
+export {categoriesRouter} from "./categories.router";
+export {mainPageRouter} from "./main-page";
+export {registrationRouter} from "./registration.router";
+export {searchRouter} from "./search.router";
+export {signInRouter} from "./sign-in.router";
