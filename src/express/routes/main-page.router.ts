59ce1451a04da427d87348ad57599c705de31101
Artem Beresnev <artembert@gmail.com>
2020-12-20 14:58:19 +0300

    [module6-add-missed-pages]: Update main-page router to prepare data for new categories-related interfaces

--- src/express/routes/main-page.router.ts
+++ src/express/routes/main-page.router.ts
@@ -4,6 +4,11 @@ import {dataProviderService} from "../services";
 import {streamPage} from "../utils/stream-page";
 import {MainPage} from "../views/pages/MainPage";
 import {SSRError} from "../errors/ssr-error";
+import {resolveCategoriesLinks} from "../utils/resolve-categories-links";
+import {resolveLinksToCategoriesWithNumbers} from "../utils/resolve-links-to-categories-with-numbers";
+import {CategoryWithNumbers} from "../../types/category-with-numbers";
+import {CategoryWithLinksAndNumbers} from "../../types/category-with-links-and-numbers";
+import {CategoryWithLink} from "../../types/category-with-link";
 
 export const mainPageRouter = Router();
 
@@ -16,7 +21,14 @@ mainPageRouter.get(`/`, async (req: Request, res: Response, next: NextFunction) 
       dataProviderService.getCategories(),
     ]);
     if (articles !== null && categories !== null) {
-      streamPage(res, MainPage, {articles, availableCategories: categories});
+      const categoriesWithNumbers: CategoryWithNumbers[] = categories.map(item => ({...item, count: 1}));
+      const categoriesWithLinksAndNumbers: CategoryWithLinksAndNumbers[] = resolveLinksToCategoriesWithNumbers(categoriesWithNumbers);
+      const categoriesWithLinks: CategoryWithLink[] = resolveCategoriesLinks(categoriesWithNumbers);
+      streamPage(res, MainPage, {
+        articles,
+        categoriesWithLinks,
+        categoriesWithLinksAndNumbers,
+      });
     } else {
       next(new SSRError({message: `Failed to load articles`, statusCode: HttpCode.INTERNAL_SERVER_ERROR}));
     }
