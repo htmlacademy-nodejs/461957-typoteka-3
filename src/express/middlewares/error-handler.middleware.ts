1b8c1b0078fe9b0812c592e20b79175ba43db4a8
Artem Beresnev <artembert@gmail.com>
2020-12-25 21:46:45 +0300

    [module6/database-integration]: Declare types

--- src/express/middlewares/error-handler.middleware.ts
+++ src/express/middlewares/error-handler.middleware.ts
@@ -2,8 +2,9 @@ import {SSRError} from "../errors/ssr-error";
 import {streamPage} from "../utils/stream-page";
 import {ErrorPage404} from "../views/pages/ErrorPage404";
 import {ErrorPage500} from "../views/pages/ErrorPage500";
+import type {Request, Response} from "express";
 
-export function errorHandlerMiddleware(err: SSRError, req, res, next): void {
+export function errorHandlerMiddleware(err: SSRError, req: Request, res: Response): void {
   console.error(err.message);
   if (!err.statusCode) {
     err.statusCode = 500;
