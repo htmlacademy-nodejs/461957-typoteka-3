60a6b95f94310555b299e21295d5f52de030b9f2
Artem Beresnev <artembert@gmail.com>
2021-01-23 20:17:18 +0400

    [module6/tests-sqlite]: Replace PG-only operator `iLike` to support sqlite

--- src/service/server/services/data-service/search.service.ts
+++ src/service/server/services/data-service/search.service.ts
@@ -1,5 +1,5 @@
 import {IArticleModel} from "../../data-access/models/article";
-import {Model, Op} from "sequelize";
+import {Model, Sequelize} from "sequelize";
 import {ArticleSearchResult} from "../../../../types/article-search-result";
 
 export class SearchService {
@@ -8,11 +8,7 @@ export class SearchService {
   public async searchByArticlesTitle(query: string): Promise<ArticleSearchResult[] | null> {
     const articles = await this.ArticleModel.findAll<Model<ArticleSearchResult>>({
       attributes: [`id`, `title`, `createdDate`],
-      where: {
-        title: {
-          [Op.iLike]: `%${query}%`,
-        },
-      },
+      where: Sequelize.where(Sequelize.fn(`LOWER`, Sequelize.col(`title`)), Sequelize.fn(`LOWER`, `%${query}%`)),
     });
     return articles.map(article => article.get());
   }
