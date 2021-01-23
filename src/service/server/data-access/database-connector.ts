a3b6975f1b448958660e5c27a6a2345a7c72deb0
Artem Beresnev <artembert@gmail.com>
2021-01-23 20:19:53 +0400

    [module6/tests-sqlite]: Connect to database-in-memory in E2E-tests

--- src/service/server/data-access/database-connector.ts
+++ src/service/server/data-access/database-connector.ts
@@ -1,6 +1,7 @@
 import {getLogger} from "../../logger";
 import {databaseConnector} from "./connectors/database.connector";
 import {Sequelize} from "sequelize";
+import {ExitCode} from "../../../constants-es6";
 
 export async function connectToDatabase(): Promise<Sequelize> {
   const logger = getLogger();
@@ -13,5 +14,19 @@ export async function connectToDatabase(): Promise<Sequelize> {
   } catch (e) {
     logger.error(`Failed to establish a database connection,\n${(e as Error).toString()}`);
     throw e;
+  }
+}
+
+// eslint-disable-next-line consistent-return
+export async function connectToDatabaseInMemory(): Promise<Sequelize> {
+  try {
+    const connection = new Sequelize(`sqlite::memory:`, {logging: false});
+    console.log(`Establishing a database connection`);
+    await connection.authenticate();
+    console.log(`Connection to the database is established`);
+    return connection;
+  } catch (e) {
+    console.error(`Failed to establish a database connection,\n${(e as Error).toString()}`);
+    process.exit(ExitCode.ERROR);
   }
 }
