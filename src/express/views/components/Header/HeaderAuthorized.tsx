b5ec8af620edb31d8ae75263a39c6daaa9486667
Artem Beresnev <artembert@gmail.com>
2021-01-27 17:20:20 +0400

    [module7-task1]: SSR: create component <HeaderAuthorized>

--- src/express/views/components/Header/HeaderAuthorized.tsx
+++ src/express/views/components/Header/HeaderAuthorized.tsx
@@ -4,24 +4,25 @@ import {ClientRoutes} from "../../../../constants-es6";
 
 interface Props {}
 
-export const Header: FunctionComponent<Props> = ({}) => {
+export const HeaderAuthorized: FunctionComponent<Props> = ({}) => {
   return (
     <header className="header">
       <Logo />
       <nav className="header__nav">
         <ul className="header__nav-list">
           <li className="header__list-item">
-            <a className="header__registration" href={ClientRoutes.REGISTRATION}>
-              Регистрация
-            </a>
+            <p>Алёна Фролова</p>
           </li>
           <li className="header__list-item">
-            <a className="header__enter" href={ClientRoutes.SIGN_IN}>
-              Вход с паролем
+            <a className="header__exit" href="#">
+              Выйти
             </a>
           </li>
         </ul>
       </nav>
+      <a className="header__avatar avatar">
+        <img src="https://via.placeholder.com/50x50.webp" alt="аватар пользователя" />
+      </a>
       <a
         className="header__search button button--search"
         href={ClientRoutes.SEARCH.INDEX}
