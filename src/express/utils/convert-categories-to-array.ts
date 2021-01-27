09eb1cd7ce8becafad00793fe90801511b9bf8b8
Artem Beresnev <artembert@gmail.com>
2021-01-27 18:23:57 +0400

    [module7-task1]: Don't fail on article without categories

--- src/express/utils/convert-categories-to-array.ts
+++ src/express/utils/convert-categories-to-array.ts
@@ -1,5 +1,8 @@
 import {Category} from "../../types/category";
 
 export function convertCategoriesToArray(categories: Record<string, `on`>): Category[] {
+  if (!categories) {
+    return [];
+  }
   return Object.keys(categories).map(item => ({id: parseInt(item, 10), label: ``}));
 }
