e16c795cbc6d6fe63792582be35c5344680428b9
Artem Beresnev <artembert@gmail.com>
2021-02-21 16:08:04 +0300

    [module7-task2]: SignUp: add E2E-tests

--- src/service/server/routes/users.router.e2e.test.ts
+++ src/service/server/routes/users.router.e2e.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-nested-callbacks */
 import {agent as request} from "supertest";
 import {Application} from "express";
 import * as http from "http";
@@ -5,9 +6,22 @@ import {initApp} from "./tests-boilerplate/init-app";
 import {UserId} from "../../../types/user-id";
 import {IUserPreview} from "../../../types/interfaces/user-preview";
 import {IUserCreatingDoublePasswords} from "../../../types/interfaces/user-creating";
+import {ILogin} from "../../../types/interfaces/login";
 
 const validUserId: UserId = 2;
 const invalidUserId = `-1`;
+const validSignUp: ILogin = {
+  email: `zaberkirder8@usgs.gov`,
+  password: `hzgdghdglhdgklgz`,
+};
+const validUserToSignUp: IUserCreatingDoublePasswords = {
+  ...validSignUp,
+  firstName: `Lowe`,
+  lastName: `Tennant`,
+  avatar: ``,
+  roleId: 2,
+  passwordRepeated: `hzgdghdglhdgklgz`,
+};
 const validNewUser: IUserCreatingDoublePasswords = {
   email: `zaberkirder8@usgs.gov`,
   firstName: `Lowe`,
@@ -80,6 +94,35 @@ describe(`Users router`, () => {
       expect(responseKeys).toContain(`roleId`);
       expect(responseKeys).toContain(`password`);
       expect(responseKeys).toContain(`passwordRepeated`);
+    });
+  });
+
+  describe(`/login`, () => {
+    beforeAll(async () => await request(app).post(`/api/users/`).send(validUserToSignUp));
+
+    describe(`POST user`, () => {
+      test(`Should return code 200 when sign-up successfully`, async () => {
+        const res = await request(app).post(`/api/users/login`).send(validSignUp);
+        expect(res.status).toBe(200);
+      });
+      test(`Should return code 400 when pass invalid password`, async () => {
+        const res = await request(app)
+          .post(`/api/users/login`)
+          .send({
+            ...validSignUp,
+            password: `invalid-password`,
+          });
+        expect(res.status).toBe(400);
+      });
+      test(`Should return code 400 when pass non-existing email`, async () => {
+        const res = await request(app)
+          .post(`/api/users/login`)
+          .send({
+            ...validSignUp,
+            email: `non-existing-email@gmail.com`,
+          });
+        expect(res.status).toBe(400);
+      });
     });
   });
 });
