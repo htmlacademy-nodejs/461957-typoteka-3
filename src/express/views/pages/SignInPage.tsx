74252c978fce31dfd54f9943fa63a632fd6b7ea4
Artem Beresnev <artembert@gmail.com>
2021-02-21 14:23:45 +0300

    [module7-task2]: SSR: create <SingInWrapper>

--- src/express/views/pages/SignInPage.tsx
+++ src/express/views/pages/SignInPage.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent} from "react";
 import {LayoutFilled} from "../components/Layout/LayoutFilled";
-import {SignIn} from "../components/SignIn/SignIn";
+import {SignInWrapper} from "../components/SignInWrapper/SignInWrapper";
 
 interface Props {
   endPoint: string;
@@ -9,12 +9,27 @@ interface Props {
 export const SignInPage: FunctionComponent<Props> = ({endPoint}) => {
   return (
     <LayoutFilled pageTitle={`Вход`}>
-      <main>
-        <h1 className="visually-hidden">Войти</h1>
-        <section>
-          <SignIn endPoint={endPoint} />
-        </section>
-      </main>
+      <SignInWrapper>
+        <h2 className="popup__title">Войти</h2>
+        <div className="popup__form form form--log-in">
+          <form action={endPoint} method="POST" encType="multipart/form-data">
+            <div className="form__field">
+              <label>
+                <input type="email" name="email" placeholder="Электронная почта" required />
+              </label>
+            </div>
+            <div className="form__field">
+              <label>
+                <input type="password" name="password" placeholder="Пароль" required />
+              </label>
+            </div>
+            <p className="form__error-message">Некоторая ошибка произошла, беда!</p>
+            <button className="form__submit-btn form__submit-btn--log-in button button--colored" type="submit">
+              Войти
+            </button>
+          </form>
+        </div>
+      </SignInWrapper>
     </LayoutFilled>
   );
 };
