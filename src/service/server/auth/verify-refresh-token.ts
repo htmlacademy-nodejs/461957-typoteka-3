30b1902ee630911fe51ec8ea977e859f49b7c7ac
Artem Beresnev <artembert@gmail.com>
2021-05-12 09:44:55 +0300

    [module7-task3]: [API]: Fix payload extraction from refresh token. Previously new token contains 'iat' & 'exp' fields from the past

--- src/service/server/auth/verify-refresh-token.ts
+++ src/service/server/auth/verify-refresh-token.ts
@@ -5,11 +5,21 @@ import {verify} from "jsonwebtoken";
 
 export async function verifyRefreshToken(token: string): Promise<IUserPreview> {
   return new Promise((resolve, reject) => {
-    verify(token, ENV.JWT_REFRESH_SECRET, (err, userData) => {
+    verify(token, ENV.JWT_REFRESH_SECRET, (err, userData: IUserPreview & unknown) => {
       if (err) {
         reject(HttpCode.FORBIDDEN);
       }
-      resolve(userData as IUserPreview);
+      resolve(extractUserFromToken(userData));
     });
   });
+}
+
+function extractUserFromToken(refreshToken: IUserPreview & unknown): IUserPreview {
+  return {
+    id: refreshToken.id,
+    avatar: refreshToken.avatar,
+    firstName: refreshToken.firstName,
+    lastName: refreshToken.lastName,
+    roleId: refreshToken.roleId,
+  };
 }
