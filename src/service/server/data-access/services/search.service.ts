96f8d7a11b41e8741c5d706f4d1a75b8911da352
Artem Beresnev <artembert@gmail.com>
2021-05-30 12:57:55 +0300

    [feature/comments-refactoring]: [Fix] Fix search

--- src/service/server/data-access/services/search.service.ts
+++ src/service/server/data-access/services/search.service.ts
@@ -1,5 +1,5 @@
 import {IArticleModel} from "../models/article";
-import {Model, Sequelize} from "sequelize";
+import {Model, Op, Sequelize} from "sequelize";
 import {ArticleSearchResult} from "../../../../types/article-search-result";
 
 export class SearchService {
@@ -8,7 +8,9 @@ export class SearchService {
   public async searchByArticlesTitle(query: string): Promise<ArticleSearchResult[] | null> {
     const articles = await this.ArticleModel.findAll<Model<ArticleSearchResult>>({
       attributes: [`id`, `title`, `createdDate`],
-      where: Sequelize.where(Sequelize.fn(`LOWER`, Sequelize.col(`title`)), Sequelize.fn(`LOWER`, `%${query}%`)),
+      where: Sequelize.where(Sequelize.fn(`LOWER`, Sequelize.col(`title`)), {
+        [Op.like]: `%${query.toLowerCase()}%`,
+      }),
     });
     return articles.map(article => article.get());
   }
