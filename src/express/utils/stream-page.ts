c1dd0223475582262b7840e1d827e23fdc0cbdc4
Artem Beresnev <artembert@gmail.com>
2021-06-21 00:53:25 +0300

    [feature/build-frontend]: [Feature] Add @fluentui/react

--- src/express/utils/stream-page.ts
+++
@@ -1,8 +0,0 @@
-import {Response} from "express";
-import React, {FunctionComponent, PropsWithChildren} from "react";
-import ReactDOMServer from "react-dom/server";
-
-export function streamPage<T>(res: Response, page: FunctionComponent<T>, props?: PropsWithChildren<T>): void {
-  res.write(`<!DOCTYPE html>`);
-  ReactDOMServer.renderToStaticNodeStream(React.createElement(page, props)).pipe(res);
-}
