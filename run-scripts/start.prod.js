dfe548f8b08b5aad0939ac0c42085e36e1f3ad0a
Artem Beresnev <artembert@gmail.com>
2021-06-23 01:19:31 +0300

    [feature/concurrently]: [Feature] add scripts to run app in parallel

---
+++ run-scripts/start.prod.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const concurrently = require(`concurrently`);
+
+concurrently(
+  [
+    {command: `npm run start:api:prod`, name: `API`, prefixColor: `blue`},
+    {command: `npm run start:ssr:prod`, name: `SSR`, prefixColor: `magenta`},
+  ],
+  {
+    prefix: "name",
+    killOthers: ["failure", "success"],
+    restartTries: 3,
+  },
+);
