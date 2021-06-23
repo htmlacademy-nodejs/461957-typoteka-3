2a90aedc8f3fdabf0aefddc7d6ce15b3bab88e29
Artem Beresnev <artembert@gmail.com>
2021-06-23 20:50:41 +0300

    [feature/concurrently]: [Fix] Fix run scripts

--- copy-static-assets.js
+++
@@ -1,3 +0,0 @@
-const shell = require("shelljs");
-
-shell.cp("-R", "src/express/static", "build/express/static/");
---
+++ run-scripts/copy-static-assets.js
@@ -0,0 +1,3 @@
+const shell = require("shelljs");
+
+shell.cp("-R", "src/express/static", "build/express/static");
