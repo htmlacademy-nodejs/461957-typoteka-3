527be0f62b4db5d8623f590c04e151c025d11132
Artem Beresnev <artembert@gmail.com>
2021-07-01 11:05:51 +0300

    [feature/exports]: [Feature] Show current year in the footer

--- src/express/views/components/Footer/Footer.tsx
+++ src/express/views/components/Footer/Footer.tsx
@@ -4,16 +4,17 @@ interface FooterProps {
   isLargeIndent?: boolean;
 }
 
-const Footer: FunctionComponent<FooterProps> = ({isLargeIndent}) => (
-  <footer className={"footer" + (isLargeIndent ? " footer--padding" : "")}>
-    <a className="footer__copyright" href="#" aria-label="HTMLAcademy">
-      <img src="img/icons/HTMLAcademyLogo.svg" width="112" height="50" alt="HTMLAcademy" />
-    </a>
-    <span className="footer__dot" />
-    <p className="footer__text">Типотека, 2019</p>
-  </footer>
-);
-
-export {
-  Footer,
+const Footer: FunctionComponent<FooterProps> = ({isLargeIndent}) => {
+  const year = new Date().getFullYear();
+  return (
+    <footer className={"footer" + (isLargeIndent ? " footer--padding" : "")}>
+      <a className="footer__copyright" href="#" aria-label="HTMLAcademy">
+        <img src="img/icons/HTMLAcademyLogo.svg" width="112" height="38" alt="HTMLAcademy" />
+      </a>
+      <span className="footer__dot" />
+      <p className="footer__text">Типотека, {year}</p>
+    </footer>
+  );
 };
+
+export {Footer};
