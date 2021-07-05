74942764101ec66c9d177ee92a8dc90f12acd50d
Artem Beresnev <artembert@gmail.com>
2021-07-05 09:33:10 +0300

    [feature/popular-articles]: [API] [Fix] validate max length of comment

--- src/service/server/validators/schemas/new-comment.ts
+++ src/service/server/validators/schemas/new-comment.ts
@@ -3,15 +3,18 @@ import Joi from "joi";
 import {ICommentCreating} from "../../../../types/interfaces/comment-creating";
 
 const COMMENT_TEXT_MIN_LENGTH = 20;
+const COMMENT_TEXT_MAX_LENGTH = 255;
 
 const newCommentSchema = Joi.object<ICommentCreating>({
   text: Joi.string()
     .min(COMMENT_TEXT_MIN_LENGTH)
+    .max(COMMENT_TEXT_MAX_LENGTH)
     .required()
     .messages({
       "any.required": `Обязательное поле`,
       "string.empty": `Поле не может быть пустым`,
       "string.min": `Минимальная длина ${COMMENT_TEXT_MIN_LENGTH} символов`,
+      "string.max": `Максимальная длина ${COMMENT_TEXT_MAX_LENGTH} символов`,
     }),
   articleId: Joi.number().positive().integer().required().messages({
     "any.required": `Обязательное поле`,
@@ -22,6 +25,4 @@ const newCommentSchema = Joi.object<ICommentCreating>({
   }),
 });
 
-export {
-  newCommentSchema,
-};
+export {newCommentSchema};
