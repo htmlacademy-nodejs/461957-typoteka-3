28138003ab9030c4c8facc3014ee91ed2580c56d
Artem Beresnev <artembert@gmail.com>
2021-07-31 20:15:51 +0300

    [feature/static]: [SSR] [refactor] Combine header for error pages into one

--- src/express/views/pages/error-page-404.tsx
+++ src/express/views/pages/error-page-404.tsx
@@ -1,11 +1,11 @@
 import React, {FunctionComponent} from "react";
 
 import {Footer} from "../components/footer/footer";
-import {Header404} from "../components/header/header-404";
 import {Layout} from "../components/layout/layout";
+import {HeaderError} from "../components/header/header-error";
 
-const ErrorPage404: FunctionComponent = ({}) => (
-  <Layout pageTitle={`Ошибка`} header={<Header404 />} wrapperMode={"error"} footer={<Footer />}>
+const ErrorPage404: FunctionComponent = () => (
+  <Layout pageTitle={`Ошибка`} header={<HeaderError type="404" />} wrapperMode={"error"} footer={<Footer />}>
     <h1 className="visually-hidden">Ошибка 404</h1>
     <section className="error">
       <h2 className="error__title title title--big">Похоже ошиблись адресом</h2>
